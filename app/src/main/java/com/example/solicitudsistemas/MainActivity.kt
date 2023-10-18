71a1f93003b6d14f7508cee01a26c8cd295676ec
adisa <adisautean@gmail.com>
2023-10-18 22:19:04 +0200

    Obtenir dades del formulari i mostrar amb un binding a la pantalla

--- app/src/main/java/com/example/solicitudsistemas/MainActivity.kt
+++ app/src/main/java/com/example/solicitudsistemas/MainActivity.kt
@@ -3,10 +3,22 @@ package com.example.solicitudsistemas
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.preference.EditTextPreference
+import android.view.View
 import android.view.WindowManager
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.core.content.ContextCompat
+import com.example.solicitudsistemas.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
+    lateinit var user: EditText
+    lateinit var pwd: EditText
+    lateinit var login_btn: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -16,5 +28,19 @@ class MainActivity : AppCompatActivity() {
             window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
             window.statusBarColor = ContextCompat.getColor(this, R.color.black)
         }
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.loginBtn.setOnClickListener(View.OnClickListener {
+            if (binding.user.text.toString() == "AS" && binding.pwd.text.toString() == "pk8jioM9"){
+                Toast.makeText(this, "Loguejat!", Toast.LENGTH_SHORT).show()
+            }else{
+                Toast.makeText(this, "Error!", Toast.LENGTH_SHORT).show()
+
+            }
+        })
+
     }
+
 }
