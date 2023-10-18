3ee8ac9ef7e85dac6f993df4ac22011f3bf6452d
AS <AS@FLOWER.local>
2023-10-18 15:47:49 +0200

    Initial commit

---
+++ app/src/main/java/com/example/solicitudsistemas/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.solicitudsistemas
+
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.WindowManager
+import androidx.core.content.ContextCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            val window = this.window
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+            window.statusBarColor = ContextCompat.getColor(this, R.color.black)
+        }
+    }
+}
